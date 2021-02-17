a4a199273aced21f64a36d11b674be844e863f3e
Paul VandenBroeck <PVB96P@gmail.com>
2021-02-17 17:21:45 -0500

    Forgot to add setup.bat for the legacy versions 2.0B - 8.0

---
+++ Legacy Versions/setup.bat
@@ -0,0 +1,10 @@
+@ECHO off 
+title PSE setup
+ECHO Do you wish to install PSE files? 
+ECHO Please close this window if you do not.
+Pause
+cls
+MD "C:\PSE"
+ECHO.> "C:\PSE\Blank.txt"
+ECHO Setup complete!
+pause
